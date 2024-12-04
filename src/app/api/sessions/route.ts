fe2aea090492fad2fdef75ce350655aa77087d14
yuni <lyhhh0320@gmail.com>
2024-12-05 01:18:32 +0900

    fix: delete middleware

--- src/app/api/sessions/route.ts
+++ src/app/api/sessions/route.ts
@@ -1,23 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { OpenVidu } from 'openvidu-node-client';
-import Cors from 'cors';
-
-// Initialize CORS middleware
-const cors = Cors({
-  origin: '*',
-});
-
-// Helper method to wait for a middleware to execute before continuing
-function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
 
 // export default async function handler(
 //   req: NextApiRequest,
@@ -63,9 +45,7 @@ const OPENVIDU_URL = process.env.OPENVIDU_URL || 'http://localhost:4443/';
 const OPENVIDU_SECRET = process.env.OPENVIDU_SECRET || 'MY_SECRET';
 const openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
 
-export async function POST(req: NextRequest, res: NextResponse) {
-  await runMiddleware(req, res, cors);
-
+export async function POST(req: NextRequest) {
   const requestBody = await req.json();
   const { customSessionId } = requestBody;
 
