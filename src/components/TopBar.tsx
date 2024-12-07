cb91fca3169a15c3ad59e110615f7a6d20be9a2b
yuni <lyhhh0320@gmail.com>
2024-12-08 04:33:54 +0900

    fix: ssr error

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,10 +5,19 @@ import Logo from '../../public/svgs/logo.svg';
 import Profile from '../../public/svgs/profile.svg';
 import Button from './Button';
 import { useHotkeys } from 'react-hotkeys-hook';
+import { useEffect, useState } from 'react';
 
 export function TopBar() {
   const pathname = usePathname();
   const router = useRouter();
+
+  const [isShortcut, setIsShortcut] = useState(false);
+
+  useEffect(() => {
+    if (window.localStorage.getItem('shortcut') === 'true') {
+      setIsShortcut(true);
+    }
+  }, []);
 
   // 로그아웃
   const handleSignOut = () => {
@@ -19,7 +28,7 @@ export function TopBar() {
 
   // 로그아웃 단축키: x
   useHotkeys('x', handleSignOut, {
-    enabled: window.localStorage.getItem('shortcut') === 'true',
+    enabled: isShortcut,
   });
 
   return (
@@ -40,7 +49,7 @@ export function TopBar() {
       ) : (
         <Button
           onClick={handleSignOut}
-          className="hover:!bg-white-hover-2 !h-fit !border !border-primary !bg-[#ffffff] p-2 !text-primary"
+          className="!h-fit !border !border-primary !bg-[#ffffff] p-2 !text-primary hover:!bg-white-hover-2"
         >
           로그아웃
         </Button>
